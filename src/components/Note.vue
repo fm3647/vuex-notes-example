f272fc3590d802c2f525d6946ff27824f379973d
ICE <ice@ICEs-iMac.local>
2017-10-31 17:21:19 +0900

    update

---
+++ src/components/Note.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+  <div class="item">
+    <div class="meta">
+      {{ 'updated' }}
+    </div>
+    <div class="content">
+      <div class="header">
+        {{ 'header' || '새글쓰기' }}
+      </div>
+      <div class="extra">
+        <Editor/>
+        {{ 'words' }}자
+        <i class="right floated trash outline icon"></i>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Editor from './Editor'
+export default {
+  components: {
+    Editor
+  }
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ src/components/Notes.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+  <div class="ui container notes">
+    <h4 class="ui horizontal divider header">
+      <i class="paw icon"></i>
+      Suuon Notes App _ Vue.js
+    </h4>
+    <a href="" class="ui right floated basic violet button">글쓰기</a>
+    <div class="ui divided items">
+      <note></note>
+      <span class="ui small disabled header">작성글이 없네요, 글쓰기를 클릭하시면 내용입력을 할 수 있습니다.</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import Note from './Note'
+export default {
+  components: {
+    Note
+  }
+}
+</script>
+
+<style lang="css">
+</style>
